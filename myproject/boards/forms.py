6e9f1652b712419e71b6fabfcba3631d519eddeb
Jack Koontz <a66062@gmail.com>
2020-08-01 16:19:44 -0500

    first com

---
+++ myproject/boards/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+
+from .models import Topic
+
+
+class NewTopicForm(forms.ModelForm):
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'rows': 5, 'placeholder': 'Let em have it'}
+        ),
+        max_length=4000,
+        help_text='Max length 4000.'
+    )
+
+    class Meta:
+        model = Topic
+        fields = ['subject', 'message']
